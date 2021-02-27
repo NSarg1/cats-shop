8742592a158b3df05c29d0dbde42f56360d9ba95
Narek <nsarg7@gmail.com>
2021-02-27 10:43:35 +0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,9 @@
+// const printName = (name) => {
+//     console.log(name);
+// };
+
+// printName({ name: "Ashot" });
+
+const arr = ["Marvin", "Darvin", "Harry Potter"];
+
+console.log(arr.join(","));
