5537b9f8b4e45cb06a0a535c907e827805f5b812
Andran1 <Andran1@users.noreply.github.com>
2021-03-09 22:09:50 +0400

    Added nothing, but learnt some JS

--- src/App.js
+++ src/App.js
@@ -8,134 +8,146 @@ import CardItem from "./components/card-item/CardItem.component";
 import "./app.scss";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            search: "",
-            cats: [
-                {
-                    id: 1,
-                    name: "Marvin",
-                    email: "marvin@mail.com",
-                    // url: "https://robohash.org/1?set=set4&size=180x180",
-                },
-                {
-                    id: 2,
-                    name: "Darvin",
-                    email: "darvin@mail.com",
-                    // url: "https://robohash.org/2?set=set4&size=180x180",
-                },
-                {
-                    id: 3,
-                    name: "Harry Potter",
-                    email: "harry@mail.com",
-                    // url: "https://robohash.org/3?set=set4&size=180x180",
-                },
-                {
-                    id: 4,
-                    name: "Kitty",
-                    email: "harry@mail.com",
-                    // url: "https://robohash.org/3?set=set4&size=180x180",
-                },
-                {
-                    id: 5,
-                    name: "Petya",
-                    email: "harry@mail.com",
-                    // url: "https://robohash.org/3?set=set4&size=180x180",
-                },
-            ],
-            catsFromServer: [],
-        };
-
-        console.log("Constructor...");
-    }
-
-    handleChange = (event) => {
-        //? INPUTVALUE
-        const searchValue = event.target.value;
-
-        //? SET INPUT VALUE TO STATE
-        this.setState({ search: searchValue });
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      cats: [
+        {
+          id: 1,
+          name: "Marvin",
+          email: "marvin@mail.com",
+          // url: "https://robohash.org/1?set=set4&size=180x180",
+        },
+        {
+          id: 2,
+          name: "Darvin",
+          email: "darvin@mail.com",
+          // url: "https://robohash.org/2?set=set4&size=180x180",
+        },
+        {
+          id: 3,
+          name: "Harry Potter",
+          email: "harry@mail.com",
+          // url: "https://robohash.org/3?set=set4&size=180x180",
+        },
+        {
+          id: 4,
+          name: "Kitty",
+          email: "harry@mail.com",
+          // url: "https://robohash.org/3?set=set4&size=180x180",
+        },
+        {
+          id: 5,
+          name: "Petya",
+          email: "harry@mail.com",
+          // url: "https://robohash.org/3?set=set4&size=180x180",
+        },
+      ],
+      catsFromServer: [],
     };
 
-    componentDidMount() {
-        console.log("componentDidMount...");
-        this.setState({ isLoading: true });
-        const response = axios.get("https://jsonplaceholder.typicode.com/users");
+    console.log("Constructor...");
+  }
 
-        response
-            .then((result) => {
-                const data = result.data;
-                this.setState({ catsFromServer: data, isLoading: false });
-                // console.log("OK");
-            })
-            .catch((err) => {
-                setTimeout(() => {
-                    this.setState({
-                        errorMessage: "Something went wrong",
-                        isLoading: false,
-                    });
-                }, 2000);
-                // console.log("չOK");
-            });
-    }
+  handleChange = (event) => {
+    //? INPUTVALUE
+    const searchValue = event.target.value;
 
-    deleteCatHandle = (id) => {
-        const cats = [...this.state.catsFromServer];
+    //? SET INPUT VALUE TO STATE
+    this.setState({ search: searchValue });
+  };
 
-        const selectedUserIndex = this.state.catsFromServer.findIndex((item, idx) => {
-            return item.id === id;
-        });
-        console.log(selectedUserIndex);
-        cats.splice(selectedUserIndex, 1);
-        console.log(cats);
-        this.setState({ catsFromServer: cats });
-    };
+  componentDidMount() {
+    console.log("componentDidMount...");
+    this.setState({ isLoading: true });
+    const response = axios.get("https://jsonplaceholder.typicode.com/users");
 
-    render() {
-        console.log("render...");
-        const mySearchInputValue = this.state.search.toLowerCase();
-        const filteredCatsData = this.state.catsFromServer.filter((cat) => {
-            const catLowerCaseName = cat.name.toLowerCase();
-            return catLowerCaseName.includes(mySearchInputValue);
-        });
+    response
+      .then((result) => {
+        const data = result.data;
+        this.setState({ catsFromServer: data, isLoading: false });
+        // console.log("OK");
+      })
+      .catch((err) => {
+        setTimeout(() => {
+          this.setState({
+            errorMessage: "Something went wrong",
+            isLoading: false,
+          });
+        }, 2000);
+        // console.log("չOK");
+      });
+  }
 
-        return (
-            <div className="app">
-                <h1 className="app__header">Catty shop</h1>
+  deleteCatHandle = (id) => {
+    const cats = [...this.state.catsFromServer];
 
-                <div className="app__search">
-                    <input
-                        onChange={(event) => this.handleChange(event)}
-                        value={this.state.search}
-                        className="app__search-input"
-                        type="text"
-                        name="search"
-                        placeholder="Search cats"
-                    />
-                </div>
+    const selectedUserIndex = this.state.catsFromServer.findIndex(
+      (item, idx) => {
+        return item.id === id;
+      }
+    );
+    console.log(selectedUserIndex);
+    cats.splice(selectedUserIndex, 1);
+    console.log(cats);
+    this.setState({ catsFromServer: cats });
+  };
 
-                <main className="app__main">
-                    <ul className="app__main-list">
-                        {!!this.state.catsFromServer.length &&
-                            filteredCatsData.map((cat) => {
-                                return (
-                                    <CardItem
-                                        key={cat.id}
-                                        cat={cat}
-                                        deleteCatHandle={this.deleteCatHandle}
-                                    />
-                                );
-                            })}
-                    </ul>
-                </main>
+  //   deleteCatHandle = (id) => {
+  //     const newCatsFromSerfer = this.state.catsFromServer.filter(
+  //       (catFromServer) => {
+  //         return catFromServer.id !== id;
+  //       }
+  //     );
 
-                {this.state.isLoading && <div>Loading...</div>}
+  //     this.setState({ catsFromServer: newCatsFromSerfer });
+  //   };
 
-                {this.state.errorMessage && <div>{this.state.errorMessage}</div>}
-            </div>
-        );
-    }
+  render() {
+    console.log("render...");
+    const mySearchInputValue = this.state.search.toLowerCase();
+    const filteredCatsData = this.state.catsFromServer.filter((cat) => {
+      const catLowerCaseName = cat.name.toLowerCase();
+      return catLowerCaseName.includes(mySearchInputValue);
+    });
+
+    return (
+      <div className="app">
+        <h1 className="app__header">Catty shop</h1>
+
+        <div className="app__search">
+          <input
+            onChange={(event) => this.handleChange(event)}
+            value={this.state.search}
+            className="app__search-input"
+            type="text"
+            name="search"
+            placeholder="Search cats"
+          />
+        </div>
+
+        <main className="app__main">
+          <ul className="app__main-list">
+            {!!this.state.catsFromServer.length &&
+              filteredCatsData.map((cat) => {
+                return (
+                  <CardItem
+                    key={cat.id}
+                    cat={cat}
+                    deleteCatHandle={this.deleteCatHandle}
+                  />
+                );
+              })}
+          </ul>
+        </main>
+
+        {this.state.isLoading && <div>Loading...</div>}
+
+        {this.state.errorMessage && <div>{this.state.errorMessage}</div>}
+      </div>
+    );
+  }
 }
 
 export default App;
